8e380241148e9a5fa757f7b7d4a041cf0749585b
Kei Nakayama <kei.of.nakayama@gmail.com>
2023-04-23 23:36:52 +0900

    fix

--- osx/osx/ContentView.swift
+++ osx/osx/ContentView.swift
@@ -6,16 +6,41 @@
 //
 
 import SwiftUI
+import Foundation
+import MultipeerConnectivity
+
+extension LocalizedStringKey {
+
+    // This will mirror the `LocalizedStringKey` so it can access its
+    // internal `key` property. Mirroring is rather expensive, but it
+    // should be fine performance-wise, unless you are
+    // using it too much or doing something out of the norm.
+    var stringKey: String? {
+        Mirror(reflecting: self).children.first(where: { $0.label == "key" })?.value as? String
+    }
+}
+
 
 struct ContentView: View {
+    @State private var peerID: MCPeerID?
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
         }
         .padding()
+        .onAppear() {
+            if let name = Host.current().localizedName {
+                let peerID = MCPeerID(displayName: name)
+                let session = MCSession(peer: peerID)
+                let delegate = Client.init()
+                
+                session.delegate = delegate
+                self.peerID = peerID
+            }
+            
+        }
     }
 }
 
@@ -23,4 +48,23 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+class Client: NSObject, MCSessionDelegate {
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+    }
+    
+    
 }
