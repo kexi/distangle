4483c9e7408d0bb0dd87131cb05e176007fe9e3b
Kei Nakayama <kei.of.nakayama@gmail.com>
2023-04-23 22:41:29 +0900

    fix

---
+++ osx/osx/osxApp.swift
@@ -0,0 +1,17 @@
+//
+//  osxApp.swift
+//  osx
+//
+//  Created by 中山慶 on 2023/04/23.
+//
+
+import SwiftUI
+
+@main
+struct osxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
