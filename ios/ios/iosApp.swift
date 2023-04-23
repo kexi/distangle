555fcfe585132a200d401f548e3fdc9039b23a05
Kei Nakayama <kei.of.nakayama@gmail.com>
2023-04-23 22:50:22 +0900

    fix

---
+++ ios/ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by 中山慶 on 2023/04/23.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
